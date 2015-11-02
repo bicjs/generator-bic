4c0aa9937f5d5f3403e454c97b3364a96e253b5f
Ray Patterson <github.raypatterson@gmail.com>
2015-11-01 18:48:03 -0800

    feat(Development workflow): Added Commitizen
    Added Commitizen and moved some things around.
    
    They're all breaking changes.

---
+++ generators/app/src/README.md
@@ -0,0 +1,13 @@
+# title
+
+## Install project dependencies
+
+```bash
+$ npm run setup
+```
+
+## Run local development server
+
+```bash
+$ npm run local
+```
--- slushfile.js
+++ slushfile.js
@@ -2,18 +2,21 @@
 
 var path = require('path');
 var gulp = require('gulp');
+var sequence = require('gulp-sequence');
 var install = require('gulp-install');
 var conflict = require('gulp-conflict');
 var template = require('gulp-template');
 var inquirer = require('inquirer');
 var _ = require('lodash');
 
-var projectName = path.basename(process.cwd());
+var _projectName = path.basename(process.cwd());
+
+var _answers;
 
 var prompts = require('./generators/app/prompts')({
   project: {
-    name: projectName,
-    title: projectName
+    name: _projectName,
+    title: _projectName
       .split('-')
       .reduce(function(title, word) {
         title.push(_.capitalize(word));
@@ -23,33 +26,65 @@ var prompts = require('./generators/app/prompts')({
   }
 });
 
+gulp.task('copy', function() {
+
+  return gulp.src([
+      '**',
+      '!**/.DS_Store',
+      '!**/node_modules',
+      '!**/bower_components',
+      '!**/generated',
+      '!**/dist',
+      '!**/.tmp',
+      '!**/*.log'
+    ], {
+      cwd: path.join(__dirname, 'generators/app/src'),
+      dot: true
+    }) // Note use of __dirname to be relative to generator
+    .pipe(conflict('./')) // Confirms overwrites on file conflicts
+    .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
+});
+
+gulp.task('template', function() {
+
+  return gulp.src([
+      '**',
+      '!**/.DS_Store',
+      '!**/node_modules',
+      '!**/bower_components',
+      '!**/generated',
+      '!**/dist',
+      '!**/.tmp',
+      '!**/*.log'
+    ], {
+      cwd: path.join(__dirname, 'generators/app/templates'),
+      dot: true
+    }) // Note use of __dirname to be relative to generator
+    .pipe(template(_answers)) // Lodash template support
+    .pipe(conflict('./')) // Confirms overwrites on file conflicts
+    .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
+});
+
+gulp.task('install', function() {
+
+  return gulp.src([
+      'node_modules',
+      'bower_components'
+    ], {
+      cwd: './'
+    })
+    .pipe(install()); // Run `bower install` and/or `npm install` if necessary
+});
+
 gulp.task('default', function(done) {
+
   inquirer.prompt(prompts,
 
     function(answers) {
 
-      console.log('answers', answers);
+      _answers = answers;
 
-      gulp.src([
-          '**',
-          '!**/.DS_Store',
-          '!**/node_modules',
-          '!**/bower_components',
-          '!**/generated',
-          '!**/dist',
-          '!**/.tmp',
-          '!**/*.log'
-        ], {
-          cwd: path.join(__dirname, 'generators/app/templates'),
-          dot: true
-        }) // Note use of __dirname to be relative to generator
-        .pipe(template(answers)) // Lodash template support
-        .pipe(conflict('./')) // Confirms overwrites on file conflicts
-        .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
-        .pipe(install()) // Run `bower install` and/or `npm install` if necessary
-        .on('end', function() {
-          done(); // Finished!
-        })
-        .resume();
+      sequence('copy', 'template', 'install')(done);
+
     });
 });
